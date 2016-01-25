22dbc9dcde685b56b82b08607d90368be4f998a4
John Lundgren <john.paul.lundgren@gmail.com>
2016-01-25 13:39:57 +0100

    Added a new println function

--- src/testinggit/TestingGit.java
+++ src/testinggit/TestingGit.java
@@ -16,6 +16,7 @@ public class TestingGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello again");
     }
     
 }
